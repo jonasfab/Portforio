86b1935c9ea7b36cf9fdb204d07f20836864aeef
Jonas Fabricio <jonas_fab@hotmail.com>
2023-09-29 23:24:30 -0300

    commit

---
+++ register/envia.php
@@ -0,0 +1,22 @@
+<?php
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email'])
+$telefone = addslashes($_POST['telefone'])
+$mensagem = addslashes($_POST['mensagem'])
+
+$para = "jonas_fab@hotmail.com";
+
+$assunto = "Projetos de clientes. Desenvolvimento Web - Portfolio"
+
+$corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+$cabeca = "From  jonasfabricio13@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+if(mail($para,$assunto,$corpo,$cabeca)){
+    echo("E-mail enviado com sucesso!")
+}
+else {
+    echo("Houve um erro ao enviar o email!");
+}
+
+?>
