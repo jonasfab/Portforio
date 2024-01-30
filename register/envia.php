9f350cbb022792a5a2be68e156676c9a2da86c3c
Jonas Fabricio <145735433+jonasfab@users.noreply.github.com>
2024-01-29 22:52:27 -0300

    adição

--- register/envia.php
+++ register/envia.php
@@ -4,32 +4,19 @@
   $email = $_POST['email'];
   $telefone = $_POST['telefone'];
   $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
 
-  
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Telefone: </b>$telefone</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
+
   //Emails para quem será enviado o formulário
-  $destino = "jonasfabricio13@gmail.com";
+  $para = "jonasfabricio13@gmail.com";
   $assunto = "Contato pelo Portfólio";
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
 
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../regist.html'>";
+  $cabeca = "From: jonas_fab@hotmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+  if(mail($para,$assunto,$corpo,$cabeca)){
+      echo("E-mail enviado com sucesso!");
+  }else{
+      echo("Houve um erro ao enviar o email!");
+  }
 ?>
